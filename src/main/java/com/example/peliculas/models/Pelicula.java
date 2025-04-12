84c198458febb5c87e117b9ca894bd318719c894
Haim <haim.pizarro@gmail.com>
2025-04-12 19:51:28 -0400

    Modificacion y utilizacion de base de datos en oracle cloud listo/get

--- src/main/java/com/example/peliculas/models/Pelicula.java
+++ src/main/java/com/example/peliculas/models/Pelicula.java
@@ -1,22 +1,26 @@
 package com.example.peliculas.models;
 
-import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table(name = "peliculas")
+@Table(name = "PELICULA", schema = "SPRING_BOOT_E1")
 public class Pelicula {
+    
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
     private Long id;
+    
     private String titulo;
     private int anio;
     private String director;
